a6c3a405f44de43560fc230ffc98faa0dcb2c56e
abhushan20044 <165487155+abhushan20044@users.noreply.github.com>
2024-04-19 02:06:16 -0400

    Update after_scan-bad_posture.js
    added emergency exit functionality

--- after_scan-bad_posture.js
+++ after_scan-bad_posture.js
@@ -58,6 +58,11 @@ var sp2tx = {
 
 document.addEventListener('DOMContentLoaded', function () {
     setupFramesAndSocket(frames, sp2tx);
+    const emergencyExitButton = document.getElementById('emergencyExitButton');
+    emergencyExitButton.addEventListener('click', function() {
+        // Navigate to the welcome screen
+        window.location.href = "index.html"; 
+    });
 });
 
 
