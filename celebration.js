c656d12a94e30bb6b88c19fb4e0677887881209f
Duncan Du <duncan.du@yale.edu>
2024-04-22 22:16:57 -0400

    Added buffer to celebration before enabling exit

--- celebration.js
+++ celebration.js
@@ -2,7 +2,6 @@ import {emergency_exit_frames} from './emergency_exit.js'
 const SECONDS_TO_COUNTDOWN = 15;
 
 function setup() {
-  emergency_exit_frames.start()
   let countdownNumber = SECONDS_TO_COUNTDOWN;
   const countdownElement = document.getElementById('countdown');
 
@@ -11,6 +10,10 @@ function setup() {
   const intervalId = setInterval(() => {
     countdownNumber--;
     countdownElement.textContent = countdownNumber;
+    if (SECONDS_TO_COUNTDOWN - countdownNumber === 4) {
+      console.log("4 Seconds have passed, enabling emergency exit function");
+      emergency_exit_frames.start();
+    }
 
     if (countdownNumber <= 0) {
       clearInterval(intervalId);
